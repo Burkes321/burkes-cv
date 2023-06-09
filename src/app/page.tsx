ef4ac918be995cfa6f9a7b9087c86239f2faf5f0
Samuel Burke <samuel.burke@emplifi.io>
2023-06-10 01:45:30 +0200

    reusable component for header links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,33 @@ import githubLogo from '../../public/githubLogo.svg';
 import linkedinLogo from '../../public/linkedinLogo.svg';
 import gmailLogo from '../../public/gmailLogo.svg';
 
-export default function Home() {
-  return (
-    <main className={css.pageContainer}>
-      <h1 className={css.header}>Sam Burke</h1>
-      <hr className={css.headerUnderline} />
-      <div className={css.linksContainer}>
-        {/* TODO: make a component for each link with a variable width becuase of the variable text content */}
-        <Link href="https://github.com/Burkes321" className={css.linkContainer}>
-          <Image src={githubLogo} alt="github logo" width={32} height={32} />
-          <p>GitHub</p>
-        </Link>
-        <div className={css.linkContainer}>
-          <Image
-            src={linkedinLogo}
-            alt="linkedin logo"
-            width={32}
-            height={32}
-          />
-          <p>LinkedIn</p>
-        </div>
-        <div className={css.linkContainer}>
-          <Image src={gmailLogo} alt="gmail logo" width={32} height={32} />
-          <p>Email me</p>
-        </div>
-      </div>
-    </main>
-  );
-}
+import { HeaderLinkWithImage } from '~/components';
+
+const Home = () => (
+  <main className={css.pageContainer}>
+    <h1 className={css.header}>Sam Burke</h1>
+    <hr className={css.headerUnderline} />
+    <div className={css.linksContainer}>
+      <HeaderLinkWithImage
+        href="https://github.com/Burkes321"
+        image={githubLogo}
+        text="GitHub"
+        alt="github logo"
+      />
+      <HeaderLinkWithImage
+        href="https://www.linkedin.com/in/samuelburke332/"
+        image={linkedinLogo}
+        text="LinkedIn"
+        alt="linkedin logo"
+      />
+      <HeaderLinkWithImage
+        href="mailto:sam.burke99@gmail.com"
+        image={gmailLogo}
+        text="Email me"
+        alt="gmail logo"
+      />
+    </div>
+  </main>
+);
+
+export default Home;
--- src/components/HeaderLinkWithImage.tsx
+++ src/components/HeaderLinkWithImage.tsx
@@ -1,1 +1,18 @@
-export const HeaderLinkWithImage = () => {};
+import Image from 'next/image';
+import Link from 'next/link';
+
+import css from './HeaderLinkWithImage.module.scss';
+
+type Props = {
+  href: string;
+  image: any; // have to type an image as any in next. It must be a default import SVG
+  text: string;
+  alt: string;
+};
+
+export const HeaderLinkWithImage = ({ href, image, text, alt }: Props) => (
+  <Link href={href} className={css.link}>
+    <Image src={image} alt={alt} width={32} height={32} />
+    <p className={css.text}>{text}</p>
+  </Link>
+);
