687f814a2baa07e480a8208c4af3274fca79eb02
Samuel Burke <samuel.burke@emplifi.io>
2023-06-10 18:08:19 +0200

    addition of bio and experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ import { HeaderLinkWithImage } from '~/components';
 
 const Home = () => (
   <main className={css.pageContainer}>
+    {/* TODO: this could be better with a flex container for all header
+    and aligning the title to the flex start */}
     <h1 className={css.header}>Sam Burke</h1>
     <hr className={css.headerUnderline} />
     <div className={css.linksContainer}>
@@ -32,6 +34,28 @@ const Home = () => (
         text="Email me"
         alt="gmail logo"
       />
+    </div>
+    <div className={css.bio}>
+      Full-stack software engineer currently working at Emplifi in Czechia.
+      Experience working as part of a team to develop complex data analytics
+      applications for business users. Primary stack of React, Next js,
+      Typescript, React query, node, express, and postgres. Willing to learn
+      anything
+    </div>
+    <div className={css.mainContentContainer}>
+      <div className={css.experienceContainer}>
+        <h2 className={css.experienceTitle}>Experience</h2>
+        <h3 className={css.experienceSectionHeader}>Emplifi</h3>
+        {/* TODO: want to put a company logo on each */}
+        <p className={css.experienceMeta}>
+          Software Engineer <br />
+          Prague, Czechia <br />
+          January 2023 - Present
+        </p>
+      </div>
+      <div>
+        <h2 className={css.skillsTitle}>Skills</h2>
+      </div>
     </div>
   </main>
 );
